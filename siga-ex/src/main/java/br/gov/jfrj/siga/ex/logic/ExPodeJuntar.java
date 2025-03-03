2088bdb669e1934745540cccb6fe22182666892d
Bruno Casas <brunocasas04@gmail.com>
2025-03-02 21:56:16 -0400

    T158955: Correction for permission to join an authorized user

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/logic/ExPodeJuntar.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/logic/ExPodeJuntar.java
@@ -1,13 +1,16 @@
 package br.gov.jfrj.siga.ex.logic;
 
+import br.gov.jfrj.siga.cp.CpConfiguracaoCache;
 import br.gov.jfrj.siga.cp.logic.CpNaoENulo;
 import br.gov.jfrj.siga.dp.DpLotacao;
 import br.gov.jfrj.siga.dp.DpPessoa;
-import br.gov.jfrj.siga.ex.ExDocumento;
-import br.gov.jfrj.siga.ex.ExMobil;
+import br.gov.jfrj.siga.ex.*;
+import br.gov.jfrj.siga.ex.bl.Ex;
 import br.gov.jfrj.siga.ex.model.enm.ExTipoDeConfiguracao;
 import br.gov.jfrj.siga.ex.model.enm.ExTipoDeMovimentacao;
 import com.crivano.jlogic.*;
+
+import java.util.Objects;
 
 public class ExPodeJuntar extends CompositeExpressionSupport {
 
@@ -15,18 +18,18 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
     private ExDocumento docPai;
     private final DpPessoa titular;
     private final DpLotacao lotaTitular;
+    private final ExDocumento doc;
 
     public ExPodeJuntar(ExMobil mob, DpPessoa titular, DpLotacao lotaTitular) {
         this.mob = mob;
         this.titular = titular;
         this.lotaTitular = lotaTitular;
+        this.doc = mob.getDoc();
     }
 
     public ExPodeJuntar(ExDocumento docPai, ExMobil mob, DpPessoa titular, DpLotacao lotaTitular) {
-        this.mob = mob;
+        this(mob, titular, lotaTitular);
         this.docPai = docPai;
-        this.titular = titular;
-        this.lotaTitular = lotaTitular;
     }
 
     /**
@@ -45,6 +48,42 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
      */
     @Override
     protected Expression create() {
+        Expression movExpression = null;
+        ExPodePorConfiguracao roleExpression = new ExPodePorConfiguracao(titular, lotaTitular)
+                .withIdTpConf(ExTipoDeConfiguracao.MOVIMENTAR)
+                .withExTpMov(ExTipoDeMovimentacao.JUNTADA)
+                .withExMod(mob.doc().getExModelo());
+
+        if (Objects.nonNull(doc)) {
+            for (ExMovimentacao mov : doc.getMobilGeral().getExMovimentacaoSet()) {
+                if (mov.isCancelada() || mov.getExTipoMovimentacao() != ExTipoDeMovimentacao.VINCULACAO_PAPEL)
+                    continue;
+
+                if (!(Objects.equals(mov.getSubscritor(), titular) && mov.getExPapel().getIdPapel() == ExPapel.PAPEL_AUTORIZADO))
+                    continue;
+
+                ExConfiguracao config = new ExConfiguracao();
+                config.setDpPessoa(titular);
+                config.setLotacao(lotaTitular);
+                config.setExPapel(mov.getExPapel());
+                config.setExModelo(doc.getExModelo());
+                config.setCpTipoConfiguracao(ExTipoDeConfiguracao.MOVIMENTAR);
+                config.setExTipoMovimentacao(ExTipoDeMovimentacao.JUNTADA);
+
+                CpConfiguracaoCache cfg = Ex.getInstance().getConf().buscaConfiguracao(config, new int[]{0}, null);
+
+                if (Objects.nonNull(cfg)) {
+                    roleExpression.withExPapel(mov.getExPapel());
+                    movExpression = roleExpression;
+                }
+
+                break;
+            }
+        }
+
+        if (Objects.isNull(movExpression))
+            movExpression = new ExPodeMovimentar(mob, titular, lotaTitular);
+
         return And.of(
                 new ExEMobilVia(mob),
                 Not.of(new ExEstaPendenteDeAnexacao(mob)),
@@ -60,7 +99,8 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
                                 new CpNaoENulo(docPai, "documento onde foi autuado"),
                                 new ExEMobilAutuado(docPai, mob)
                         ),
-                        new ExPodeMovimentar(mob, titular, lotaTitular)),
+                        movExpression
+                ),
                 Or.of(
                         Not.of(new ExEstaPendenteDeAssinatura(mob.doc())),
                         new ExEInternoCapturado(mob.doc())
@@ -71,10 +111,7 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
                 Not.of(new ExEstaSobrestado(mob)),
                 Not.of(new ExEstaSemEfeito(mob.doc())),
                 Not.of(new ExEstaEmTramiteParalelo(mob)),
-                new ExPodePorConfiguracao(titular, lotaTitular)
-                        .withIdTpConf(ExTipoDeConfiguracao.MOVIMENTAR)
-                        .withExTpMov(ExTipoDeMovimentacao.JUNTADA)
-                        .withExMod(mob.doc().getExModelo())
+                roleExpression
         );
     }
 }
