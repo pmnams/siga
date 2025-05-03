7a3c270c6647ea770a83a31adbbe267ce5923aa1
Bruno Casas <bruno.casas@estudante.ifms.edu.br>
2025-05-03 19:18:35 -0400

    Revert "T158955: Adjustments to responsible party verification"
    This reverts commit 2acbbd15071b652d289ddbfcf809216689c560f3.

--- siga-ex/src/main/java/br/gov/jfrj/siga/ex/logic/ExPodeJuntar.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/ex/logic/ExPodeJuntar.java
@@ -48,11 +48,11 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
      */
     @Override
     protected Expression create() {
+        Expression movExpression = null;
         ExPodePorConfiguracao roleExpression = new ExPodePorConfiguracao(titular, lotaTitular)
                 .withIdTpConf(ExTipoDeConfiguracao.MOVIMENTAR)
                 .withExTpMov(ExTipoDeMovimentacao.JUNTADA)
                 .withExMod(mob.doc().getExModelo());
-        boolean titularAutorizado = false;
 
         if (Objects.nonNull(doc)) {
             for (ExMovimentacao mov : doc.getMobilGeral().getExMovimentacaoSet()) {
@@ -74,12 +74,15 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
 
                 if (Objects.nonNull(cfg)) {
                     roleExpression.withExPapel(mov.getExPapel());
-                    titularAutorizado = true;
+                    movExpression = roleExpression;
                 }
 
                 break;
             }
         }
+
+        if (Objects.isNull(movExpression))
+            movExpression = new ExPodeMovimentar(mob, titular, lotaTitular);
 
         return And.of(
                 new ExEMobilVia(mob),
@@ -96,7 +99,7 @@ public class ExPodeJuntar extends CompositeExpressionSupport {
                                 new CpNaoENulo(docPai, "documento onde foi autuado"),
                                 new ExEMobilAutuado(docPai, mob)
                         ),
-                        new ExPodeMovimentar(mob, titular, lotaTitular, titularAutorizado)
+                        movExpression
                 ),
                 Or.of(
                         Not.of(new ExEstaPendenteDeAssinatura(mob.doc())),
